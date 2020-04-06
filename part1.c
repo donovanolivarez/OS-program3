0669d7aeacc76ef24f9afa1374fb2793d1a384a8
Donovan Olivarez <donovanolivarez97@gmail.com>
2020-04-06 12:42:12 -0500

    part1.c upload

---
+++ part1.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char * argv[]){
+    // read infile with 8 bytes offset. 
+    // print out to verify that file is being read correctly 
+    return 0;
+}
+
